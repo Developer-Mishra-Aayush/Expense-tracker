d9c98e4ed7daccb28c8a1aff087d7786d661dbe0
Aayush Mishra <ayush28014@gmail.com>
2025-09-21 12:45:35 +0530

    Allowing all origin to access this site

--- tracker/tracker/settings.py
+++ tracker/tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5d^w73%tl_p9lkju_fwtii8=*pkjn9w_^acrjo*7#xcop_ysck
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
